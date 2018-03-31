1d983bb16ef357b25d0502201ccd9e80775317d8
Baha <bahadylbekov>
2018-03-31 15:58:10 +0300

    10

--- src/components/Sidebar/Profile/Profile.js
+++ src/components/Sidebar/Profile/Profile.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import ProfileAvatar from './ProfileAvatar';
 
 const Profile = (props) => (
     <div className="profile">
         <div className="profile-avatar">
-            <ProfileAvatar />
         </div>
         <p className="profile-title">Leah Stevens</p>
     </div>
