757cc4fb54586070b1e8571aebf22d1e9c2c010f
Baha <bahadylbekov>
2018-03-31 14:47:44 +0300

    1

--- src/components/PageContainer/PageContainer.js
+++ src/components/PageContainer/PageContainer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar';
 import Dashboard from '../Pages/Dashboard/Dashboard';
+import DashboardContracts from '../Pages/DashboardContracts/DashboardContracts';
 
 class PageContainer extends React.Component {
     render() {
         return (
             <div className="page-container">
                 <Navbar />
-                <Dashboard />
+                <DashboardContracts />
             </div>
         );
     }
