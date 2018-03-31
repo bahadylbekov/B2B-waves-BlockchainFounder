2027bd5ebda5fc528261fbb022121f89fc542459
Baha <bahadylbekov>
2018-03-31 15:36:31 +0300

    8

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 // import validator from 'validator';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Application from './components/Application';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-
-const App = () => (
-    <MuiThemeProvider>
+class App extends React.Component {
+  render () {
+    return (
       <Application />
-    </MuiThemeProvider>
-  );
-    
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
