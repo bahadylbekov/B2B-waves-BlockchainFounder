02e0ac215158808ab720154aa7a6f420d0a6092b
Baha <bahadylbekov>
2018-03-31 17:54:43 +0300

    add some values

--- src/components/Pages/DashboardContracts/Table.js
+++ src/components/Pages/DashboardContracts/Table.js
@@ -5,6 +5,7 @@ export default class ContractsTable extends React.Component {
     state = {
         data:[{
             number: "14-201",
+            logo: "logo-coca-cola",
             supplier: "The Coca-Cola Company",
             item: "Aluminium can",
             qty: 10,
@@ -14,6 +15,7 @@ export default class ContractsTable extends React.Component {
             id: 1,
         }, {
             number: "12-FB1",
+            logo: "logo-sberbank",
             supplier: "Sberbank Company",
             item: "Mortage Credits",
             qty: 500,
@@ -23,6 +25,7 @@ export default class ContractsTable extends React.Component {
             id: 2,
         }, {
             number: "12-PC2",
+            logo: "logo-pepsi",
             supplier: "Pepsi & Co",
             item: "Pepsi bottles",
             qty: 12,
@@ -30,6 +33,16 @@ export default class ContractsTable extends React.Component {
             totalPrice: 32,
             status: 3, 
             id: 3,
+        }, {
+            number: "12-PC2",
+            logo: "",
+            supplier: "Pepsi & Co",
+            item: "Pepsi bottles",
+            qty: 12,
+            date: "03/03/2018",
+            totalPrice: 32,
+            status: 1, 
+            id: 4,
         }]
     }
 
@@ -75,7 +88,11 @@ class TableRow extends React.Component {
         return (
             <tr className="contracts-table-row">
                 <td className="contracts-table-row-item main">{this.props.data.number}</td>
-                <td  className="contracts-table-row-item main">{this.props.data.supplier}</td>
+                <td  className="contracts-table-row-item main">
+                    { this.props.data.logo.length > 0 ? 
+                    <div className="item-logo-text"><span className={this.props.data.logo}/><p className="contracts-table-item-title">{this.props.data.supplier}</p></div>
+                    : this.props.data.supplier }
+                </td>
                 <td className="contracts-table-row-item ">{this.props.data.item}</td>
                 <td className="contracts-table-row-item ">{this.props.data.qty}</td>
                 <td  className="contracts-table-row-item ">{this.props.data.date}</td>
--- src/components/Sidebar.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Sidebar = (props) => (
-
-);
-
-export default Sidebar;
