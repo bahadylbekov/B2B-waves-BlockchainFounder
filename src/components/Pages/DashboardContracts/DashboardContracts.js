a6c39b9d726dcc45174b0e8997541635c70b39ac
Baha <bahadylbekov>
2018-03-31 15:06:11 +0300

    4

--- src/components/Pages/DashboardContracts/DashboardContracts.js
+++ src/components/Pages/DashboardContracts/DashboardContracts.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Buttons from '../Dashboard/Buttons/Buttons';
-import ContractsTable from './ContractsTable';
+import Table from './Table';
 
 class DashboardContracts extends React.Component {
     render() {
         return(
             <div className="dashboard-page">
             <Buttons />
-            <ContractsTable />
+            <Table />
             </div>
         );
     } 
---
+++ src/components/Pages/DashboardContracts/Table.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+export default class ContractsTable extends React.Component {
+
+    state = {
+        data:[{
+            number: "14-201",
+            supplier: "The Coca-Cola Company",
+            item: "Aluminium can",
+            qty: 10,
+            date: "15/04/2018",
+            totalPrice: 47.8,
+            status: 1, 
+            id: 1,
+        }, {
+            number: "12-FB1",
+            supplier: "Sberbank Company",
+            item: "Mortage Credits",
+            qty: 500,
+            date: "19/03/2018",
+            totalPrice: 124900,
+            status: 2, 
+            id: 2,
+        }, {
+            number: "12-PC2",
+            supplier: "Pepsi & Co",
+            item: "Pepsi bottles",
+            qty: 12,
+            date: "03/03/2018",
+            totalPrice: 32,
+            status: 3, 
+            id: 3,
+        }]
+    }
+
+    render() {
+        
+        let rows = this.state.data.map(contract =>
+        {
+            return <TableRow key={contract.id} data={contract}/>
+        });
+
+        return (
+            <div  className="contracts col-lg-12 col-xl-12">
+            <table className="contracts-table">
+                <TableHeader />
+                <tbody>{rows}</tbody>
+            </table>
+            </div>
+        );
+    }
+
+}
+
+class TableHeader extends React.Component {
+    render() {
+         return (
+            <thead className="contracts-table-header">
+            <tr>
+                <th className="contracts-header-item">Number</th>
+                <th className="contracts-header-item">Supplier</th>
+                <th className="contracts-header-item">Item</th>
+                <th className="contracts-header-item">Qty</th>
+                <th className="contracts-header-item">Date</th>
+                <th className="contracts-header-item">Total Price</th>
+                <th className="contracts-header-item"></th>
+            </tr>
+            </thead>
+        );
+    }
+}
+
+class TableRow extends React.Component {
+    render() {
+        return (
+            <tr className="contracts-table-row">
+                <td className="contracts-table-row-item main">{this.props.data.number}</td>
+                <td  className="contracts-table-row-item main">{this.props.data.supplier}</td>
+                <td className="contracts-table-row-item ">{this.props.data.item}</td>
+                <td className="contracts-table-row-item ">{this.props.data.qty}</td>
+                <td  className="contracts-table-row-item ">{this.props.data.date}</td>
+                <td className="contracts-table-row-item value">{this.props.data.totalPrice}</td>
+                <td className="contracts-table-row-item value">{this.props.data.status}</td>
+            </tr>
+        )
+    }
+}
