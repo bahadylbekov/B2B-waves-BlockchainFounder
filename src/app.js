0bc49deecd1dcf0379f1ec5bd8f02ca5131f831e
Baha <bahadylbekov>
2018-03-31 21:59:40 +0300

    avatar photo add

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 // import validator from 'validator';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Application from './components/Application';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-class App extends React.Component {
-  render () {
-    return (
+const App = (props) => (
+  <MuiThemeProvider>
       <Application />
-    )
-  }
-}
+  </MuiThemeProvider>
+)
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- src/components/Sidebar/Profile/Profile.js
+++ src/components/Sidebar/Profile/Profile.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ProfileAvatar from './ProfileAvatar';
 
 const Profile = (props) => (
     <div className="profile">
         <div className="profile-avatar">
+        <ProfileAvatar />
         </div>
         <p className="profile-title">Leah Stevens</p>
     </div>
