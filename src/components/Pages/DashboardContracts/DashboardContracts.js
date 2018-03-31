3666cf0148a3a1155fdce2282d704847dea37cfe
Baha <bahadylbekov>
2018-03-31 14:48:47 +0300

    2

--- src/components/Pages/DashboardContracts/DashboardContracts.js
+++ src/components/Pages/DashboardContracts/DashboardContracts.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Buttons from './Buttons/Buttons';
 
 class DashboardContracts extends React.Component {
     render() {
         return(
             <div className="dashboard-page">
+            <Buttons />
             </div>
         );
     } 
