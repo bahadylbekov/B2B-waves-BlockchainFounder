b554cce5f9c63d556b2c33e6849e433c75d4ccfd
Baha <bahadylbekov>
2018-04-01 09:44:36 +0300

    first sunday

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Sidebar from './Sidebar/Sidebar';
 import PageContainer from './PageContainer/PageContainer';
-import Dashboard from './Pages/Dashboard/Dashboard';
 
 class Application extends React.Component {
     render() {
--- src/components/PageContainer/PageContainer.js
+++ src/components/PageContainer/PageContainer.js
@@ -3,13 +3,14 @@ import Navbar from './Navbar/Navbar';
 import Dashboard from '../Pages/Dashboard/Dashboard';
 import DashboardContracts from '../Pages/DashboardContracts/DashboardContracts';
 import CurrentContract from '../Pages/CurrentContract/CurrentContract';
+import NewContract from '../Pages/NewContract/NewContract';
 
 class PageContainer extends React.Component {
     render() {
         return (
             <div className="page-container">
                 <Navbar />
-                <CurrentContract />
+                <Dashboard />
             </div>
         );
     }
